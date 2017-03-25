e0a5d85eb56e59eab1881c48b6b6cc70da4d040f
Tim Nguyen <ntim.bugs@gmail.com>
2017-03-25 01:37:53 +0000

    Contrast color calculations

--- js/SettingsStore.js
+++ js/SettingsStore.js
@@ -30,7 +30,27 @@ SettingsStore.prototype = {
       default: "#0087ff",
       type: "color",
       onApply(value) {
+        function toRgb(color) {
+          let r = "0x" + color[1] + color[2];
+          let g = "0x" + color[3] + color[4];
+          let b = "0x" + color[5] + color[6];
+          return [r, g, b];
+        }
+
+        function getLuminance([r, g, b]) {
+          let distanceB = 0, distanceW = 0;
+          distanceB = 0.2 * r + g * 0.7 + b * 0.1;
+          distanceW = (255 - r) * 0.2 + (255 - g) * 0.7 + (255 - b) * 0.1;
+          return distanceB > distanceW ? 1 : 0;
+        }
+
+        let contrastColor = getLuminance(toRgb(value)) === 1 ? "#000" : "#fff";
         document.documentElement.style.setProperty("--theme-highlight-color", value);
+        document.documentElement.style.setProperty(
+          "--theme-contrast-color", contrastColor
+        );
+        document.documentElement.classList.toggle(
+          "contrast-black", contrastColor == "#000");
       }
     }
   ],
