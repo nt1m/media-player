b4b5489cfc1eae9e3688cb6befb5ed03a3ca431f
MohIceAge <mohamed.hadjoudj@protonmail.com>
2017-03-26 21:23:31 +0200

    Add icon to electron app

--- main.js
+++ main.js
@@ -8,12 +8,12 @@ const url = require("url");
 let win;
 
 function createWindow() {
-  win = new BrowserWindow({width: 800, height: 600});
+  win = new BrowserWindow({width: 800, height: 600, icon: __dirname + "/img/icon.png"});
   win.setMenu(null);
   win.loadURL(url.format({
     pathname: path.join(__dirname, "./index.html"),
     protocol: "file:",
-    slashes: true
+    slashes: true,
   }));
   win.on("closed", () => win = null);
 }
